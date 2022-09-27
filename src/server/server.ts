356fcb3dbcede3818b7b078f4cc6a25ee062e238
Aaron Mazel-Gee <aaron1729@gmail.com>
2022-09-27 10:28:48 -0700

    add proxy settings to webpack

--- src/server/server.ts
+++ src/server/server.ts
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.status(200).send("Yellow world!!");
 });
 
+app.get("/api", (req, res) => {
+    res.status(200).send("this is the api route!");
+})
+
 app.listen(port, () => {
     console.log(`your server has been started at http://localhost:${port}`);
 });
