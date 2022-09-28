76da868d6087e889a132a81d3f593e13239226f2
Parker Steinberg <parker.s52@gmail.com>
2022-09-28 15:31:23 -0700

    name change

--- src/client/Components/MusicianInfo.tsx
+++ src/client/Components/MusicianInfo.tsx
@@ -24,7 +24,7 @@ const MusicianInfo = (props: any) => {
 
   return (
     <div className='flex flex-col items-center'>
-      <h2 className='text-4xl py-2'><span className='font-bold'>Musician:</span> {musicianInfo.bandName}</h2>
+      <h2 className='text-4xl py-2'><span className='font-bold'>Musician:</span> {props.currentMusician}</h2>
       <span className='text-2xl'><span className='font-bold'>Venmo Link:</span> {musicianInfo.venmoLink}</span>
       <span className='text-2xl'><span className='font-bold'>Bio:</span> {musicianInfo.musicianBio}</span>
       <hr />
