b93bd113f5d08856b2bc7b5f0a5950ffb1de7ee2
Aaron Mazel-Gee <aaron1729@gmail.com>
2022-09-28 10:47:02 -0700

    verified connection to db

--- src/server/models/models.js
+++ src/server/models/models.js
@@ -1,10 +1,20 @@
 const { Pool } = require('pg');
 
+// postgres URI for dan's DB made in elephantSQL
 const PG_URI = 'postgres://lcxzqwbt:fJr59MbexnXRTjBow7GYbSaFJIhXmAOU@jelani.db.elephantsql.com/lcxzqwbt';
 
 const pool = new Pool({
     connectionString: PG_URI
 });
+
+
+
+
+// // a test db query; note that the 2nd and 3rd arguments of pool.query are not needed
+// const tempQueryText = `SELECT * FROM public.songs`
+// pool.query(tempQueryText).then(value => console.log(value.rows));
+
+
 
 module.exports = {
     query: (text, params, callback) => {
