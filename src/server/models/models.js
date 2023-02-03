c257450c48b2dc8ed526f7b70bdb4a1a12786c06
Aaron Mazel-Gee <aaron1729@gmail.com>
2023-02-02 18:12:00 -0800

    minor tweaks

--- src/server/models/models.js
+++ src/server/models/models.js
@@ -3,14 +3,13 @@ const { Pool } = require('pg');
 // postgres URI for db (in elephantSQL)
 const PG_URI = process.env.PG_URI;
 
-
 const pool = new Pool({
     connectionString: PG_URI
 });
 
 module.exports = {
     query: (text, params, callback) => {
-        // console.log('executing the query:', text);
+        console.log('executing the query:', text);
         return pool.query(text, params, callback);
     }
 }
