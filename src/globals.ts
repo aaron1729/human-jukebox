b1c077ae9b3ad8a5ebfc3efe81213ebf3dcfda7e
Aaron Mazel-Gee <aaron1729@gmail.com>
2023-02-10 16:49:07 -0800

    replace .replaceAll with .replace and regex, so that code runs on server

--- src/globals.ts
+++ src/globals.ts
@@ -6,7 +6,7 @@ type Globals = {
 const globals: Globals = {
 
     // toggle whether to execute (potentially very) long console logs.
-    longConsoleLogs: false,
+    longConsoleLogs: true,
 
     // this should be a multiple of the limit number of songs spotify is willing to return in each API call (50 at time of writing), since we're getting songs in batches of that size anyways.
     hardCutoffForPlaylistLength: 500,
